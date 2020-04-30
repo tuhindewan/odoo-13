a053c44eec5ee65882d1d149bbb265382c6e1de9
tuhindewan <tuhinsshadow@gmail.com>
2020-05-01 01:31:54 +0600

    Create Appoinment from Wizard

--- custom_addons/om_hospital/wizards/create_appointment.py
+++ custom_addons/om_hospital/wizards/create_appointment.py
@@ -7,3 +7,10 @@ class CreateAppointment(models.TransientModel):
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     appointment_date = fields.Date(string='Date', required=True, default=datetime.today())
+
+    def create_appointment_wizard_button(self):
+        values = {
+            'patient_id': self.patient_id.id,
+            'appointment_date': self.appointment_date
+        }
+        self.env['hospital.appointment'].create(values)
